53cad8c13a30b5261f41aa1374851714b1721197
amelia949 <amelia_949@hotmail.com>
2013-09-13 00:41:47 -0500

    errores en ciclo
    errores en ciclo

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -74,7 +74,7 @@ $(document).ready(function () {
 			return self.tituloOriginal + " (" + self.pais + ")";    
 		}
 		
-		var data= funciones.data;
+		/*var data= funciones.data;
 		var nameComp ="";
 		alert("tam"+data.length);
     	self.complejos = ko.observableArray([]);
@@ -86,31 +86,10 @@ $(document).ready(function () {
 				}
 				antComp = data[x].nombreComplejo;
         	}
-        	alert(antComp +"-"+data[x].idSala+"-"+ data[x].hora);
-        	listdetail[x]= new detailFuncion(data[x].hora,data[x].idSala,data[x].asientos) ;
+        	alert(data[x].nombreComplejo +"-"+data[x].idSala+"-"+ data[x].hora);
+        	//listdetail[x]= new detailFuncion(data[x].hora,data[x].idSala,data[x].asientos) ;
     	}	
-    	/*
-    	var self = this;
-		
-		self.dias = ['Hoy', 'Jueves 12'];
-		self.chosenFolderId = ko.observable();
-		
-		self.goToFolder = function(folder) { 
-			self.chosenFolderId(folder);
-		}
-		self.goToFolder('Hoy');
-		self.titulo = "El Conjuro";
-		self.tituloOriginal = "The Conjuring";
-		this.genero = "Terror";
-		this.clasificacion = "RECOMENDADA PARA MAYORES DE 12 AÑOS";
-		this.duracion = "120";
-		this.pais = "Estados Unidos";
-		this.director = "Harald Zwart";
-		this.artistas = "Lily Collins, Jamie Campbell Bower, Lena Headey, Jonathan Rhys Meyers, Robert Sheehan, Jared Harris";
-		this.sinopsis = "Clary Fray es una adolescente ordinaria de Brooklyn hasta la noche en que conoce a Jace, un misterioso chico con tatuajes, quién resulta ser un guerrero mitad ángel conocido como cazador de sombras.Cuando Clary descubre que su madre Jocelyn ha sido raptada, desesperadamente busca la ayuda de Jace. Después de entrar en un portal dorado que la transporta a través del tiempo y espacio, en un latido de corazón, Clary se da cuenta que ella también tiene poderes para los cazadores de sombras.¿Pero puede aprovecharlos a tiempo para salvar a su madre? Adaptación cinematográfica de la saga THE MORTAL INSTRUMENTS.";
-		self.original = function() {
-			return self.tituloOriginal + " (" + self.pais + ")";    
-		}
+    	*/
 
         self.complejos = ko.observableArray([
          	new Complejo("CINE COLOMBIA PORTAL DEL QUINDÍO.",
@@ -122,13 +101,6 @@ $(document).ready(function () {
          				new detailFuncion("2013-09-10 15:10:00.0","9","v5,x7,f8,f5"),
          				new detailFuncion("2013-09-10 17:10:00.0","11","g9,v9,c3,v5")])
          	]);
-
-         /*
-		self.complejos = [
-			{ nombreComplejo: "CINE COLOMBIA PORTAL DEL QUINDÍO.", 
-				horarios:["2013-09-10 13:10:00.0","2013-09-10 15:10:00.0","2013-09-10 17:10:00.0"]},
-			{ nombreComplejo:"Cine Mil de Colombia", horarios:["2013-09-10 13:10:00.0","2013-09-10 15:10:00.0","2013-09-10 17:10:00.0"]}
-		];*/
 			
 		self.showHora = function(value){
 			return value.substring(10,16);
