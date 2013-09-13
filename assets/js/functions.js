0e9751cfb886b3c670052548b8930e1ec649ee50
amelia949 <amelia_949@hotmail.com>
2013-09-12 22:59:59 -0500

    web service
    web service json

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,9 +4,7 @@ function updateTextInput(val) {
 }
 	var detalle ="";
 $(document).ready(function () {
-	var asientos = new Array();
-	var sala = new Array();
-	var horasFuncion = new Array();
+	var listdetail = new Array();
 
 	alert("ready detalle =" +localStorage.idPelicula );
 	var surl = "http://172.16.0.76:8081/CinecoWS/rest/pelicula/"+localStorage.idPelicula+"?callback?";
@@ -38,12 +36,17 @@ $(document).ready(function () {
 	    error: function (xhr, status, error) { alert('Error funcionesPelicula !!' + error+"- " + status); }
 	    });
     }
-    function Complejo(nombreComplejo, horarios,asientos,sala) {
+
+    function detailFuncion(hora,idSala,asientos){
+    	var self=this;
+    	self.hora = hora;
+    	self.idSala= idSala;
+    	self.asientos=asientos;
+    }
+    function Complejo(nombreComplejo, AFunciones) {
 	    var self = this;
 	    self.nombreComplejo = nombreComplejo;
-	    self.horarios = horarios;
-	    self.asientos = asientos;
-	    self.sala = sala;
+	   	self.detailFuncions = ko.observableArray(AFunciones);   
 
 	}
     
@@ -64,6 +67,9 @@ $(document).ready(function () {
 		this.clasificacion = detalle.data.clasificacion;
 		this.duracion = detalle.data.duracion;
 		this.pais = detalle.data.pais;
+		this.sinopsis= detalle.data.sinopsis;
+		this.director= detalle.data.director;
+		this.artistas= detalle.data.artistas;
 		self.original = function() {
 			return self.tituloOriginal + " (" + self.pais + ")";    
 		}
@@ -74,43 +80,62 @@ $(document).ready(function () {
     	self.complejos = ko.observableArray([]);
     	for( x=0 ; x<data.length;x++){
     		if(antComp != data[x].nombreComplejo){
-    			
-    			asientos = new Array();
-				sala = new Array();
-				horasFuncion = new Array();
+    			listdetail = new Array();
 				if(x!=0) {
-					self.complejos.push(new Complejo(antComp,horasFuncion,asientos,sala));
+					self.complejos.push(new Complejo(antComp,listdetail));
 				}
 				antComp = data[x].nombreComplejo;
         	}
         	alert(antComp +"-"+data[x].idSala+"-"+ data[x].hora);
-        	asientos[x]=data[x].asientos;
-        	sala[x]=data[x].idSala;
-        	horasFuncion[x]=data[x].hora;
-    	}		
+        	listdetail[x]= new detailFuncion(data[x].hora,data[x].idSala,data[x].asientos) ;
+    	}	
+    	/*
+    	var self = this;
+		
+		self.dias = ['Hoy', 'Jueves 12'];
+		self.chosenFolderId = ko.observable();
+		
+		self.goToFolder = function(folder) { 
+			self.chosenFolderId(folder);
+		}
+		self.goToFolder('Hoy');
+		self.titulo = "El Conjuro";
+		self.tituloOriginal = "The Conjuring";
+		this.genero = "Terror";
+		this.clasificacion = "RECOMENDADA PARA MAYORES DE 12 AÑOS";
+		this.duracion = "120";
+		this.pais = "Estados Unidos";
+		this.director = "Harald Zwart";
+		this.artistas = "Lily Collins, Jamie Campbell Bower, Lena Headey, Jonathan Rhys Meyers, Robert Sheehan, Jared Harris";
+		this.sinopsis = "Clary Fray es una adolescente ordinaria de Brooklyn hasta la noche en que conoce a Jace, un misterioso chico con tatuajes, quién resulta ser un guerrero mitad ángel conocido como cazador de sombras.Cuando Clary descubre que su madre Jocelyn ha sido raptada, desesperadamente busca la ayuda de Jace. Después de entrar en un portal dorado que la transporta a través del tiempo y espacio, en un latido de corazón, Clary se da cuenta que ella también tiene poderes para los cazadores de sombras.¿Pero puede aprovecharlos a tiempo para salvar a su madre? Adaptación cinematográfica de la saga THE MORTAL INSTRUMENTS.";
+		self.original = function() {
+			return self.tituloOriginal + " (" + self.pais + ")";    
+		}
 
-        /* self.complejos = ko.observableArray([
-         	new Complejo("CINE COLOMBIA PORTAL DEL QUINDÍO.",["2013-09-10 13:10:00.0","2013-09-10 15:10:00.0","2013-09-10 17:10:00.0"]),
-         	new Complejo("Cine Mil de Colombia.",["2013-09-10 13:10:00.0","2013-09-10 15:10:00.0","2013-09-10 17:10:00.0"])
+        self.complejos = ko.observableArray([
+         	new Complejo("CINE COLOMBIA PORTAL DEL QUINDÍO.",
+         				[new detailFuncion("2013-09-10 13:10:00.0","3","f2,g3,g6,y6"),
+         				new detailFuncion("2013-09-10 15:10:00.0","4","g6,h8,n9,v0,d4,c9"),
+         				new detailFuncion("2013-09-10 17:10:00.0","6","g7,j7,j8,b9")]),
+         	new Complejo("Cine Mil de Colombia.",
+         				[new detailFuncion("2013-09-10 13:10:00.0","8","h7,b7,b8"),
+         				new detailFuncion("2013-09-10 15:10:00.0","9","v5,x7,f8,f5"),
+         				new detailFuncion("2013-09-10 17:10:00.0","11","g9,v9,c3,v5")])
          	]);
 
+         /*
 		self.complejos = [
 			{ nombreComplejo: "CINE COLOMBIA PORTAL DEL QUINDÍO.", 
 				horarios:["2013-09-10 13:10:00.0","2013-09-10 15:10:00.0","2013-09-10 17:10:00.0"]},
 			{ nombreComplejo:"Cine Mil de Colombia", horarios:["2013-09-10 13:10:00.0","2013-09-10 15:10:00.0","2013-09-10 17:10:00.0"]}
 		];*/
 			
-
-		this.muestraFunc = function() {
-			//self.seats.push(new SeatReservation("", self.availableMeals[0]));
-		}	
-		
 		self.showHora = function(value){
 			return value.substring(10,16);
 		}
 
-		this.compra=function (hora) { 
-            alert("Mostrar Detalle de " + hora);
+		this.compra=function (detalleFuncion) { 
+            alert("Mostrar Detalle de " + detalleFuncion.hora+" - "+detalleFuncion.idSala+" - " +detalleFuncion.asientos);
              
          }	
 	}
