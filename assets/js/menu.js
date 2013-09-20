4d25ba54c349ec7231de07f5c4126a1d5432116a
amelia949 <amelia_949@hotmail.com>
2013-09-20 14:04:22 -0500

    cambios
    cambios extremos

--- assets/js/menu.js
+++
@@ -1,6 +0,0 @@
-	$(function() {
-			 $('#MCartelera').click(function () {alert("cartelera");window.location = ("index.html"); });
-			 $('#MEstrenos').click(function () {alert("estrenos");window.location = ("estrenos.html"); });
-			 $('#MComplejos').click(function () {alert("complejos");window.location = ("ubicacion.html"); });
-			 $('#MFestivales').click(function () {alert("festivales");window.location = ("festivales.html"); });
-			});
